4a904bb19fec21d59fb215894848a5fbc857ee27
HurdyGutty <gaming.acount.1996@gmail.com>
2022-02-17 08:35:07 +0700

    Thêm cookie

--- do_an/admin/logout.php
+++ do_an/admin/logout.php
@@ -4,5 +4,6 @@ session_start();
 unset($_SESSION["id"]);
 unset($_SESSION["name"]);
 unset($_SESSION["access"]);
+setcookie('remember',null,-1);
 header("location:login.php");
 ?>
--- do_an/admin/process_login.php
+++ do_an/admin/process_login.php
@@ -4,10 +4,13 @@ require_once '../root/connect.php';
 
 $email = mysqli_real_escape_string($ket_noi,$_POST['email']);
 $password = mysqli_real_escape_string($ket_noi,$_POST['password']);
+$remember = mysqli_real_escape_string($ket_noi,$_POST['remember']);
 
 $regex_email = "/^[\w\-\.]+@(?:[\w-]+\.)+[\w-]{2,4}$/";
 $regex_password = "/^((?=.*[A-Z])(?=.*[0-9]).{8,}|(abc))$/";
-
+$hasspass = password_hash($password, PASSWORD_DEFAULT);
+$key_array = array("email"=>$email,"hasspass"=>$hasspass,"remember"=>$remember);
+$key_string = serialize($key_array);
 if (preg_match($regex_email, $email) == 1 && preg_match($regex_password, $password) == 1){
 
 $sql = "select * from adm_list 
@@ -16,10 +19,19 @@ where email = '$email' and  password = '$password'";
 $result = mysqli_query($ket_noi,$sql);
 if(mysqli_num_rows($result) == 1){
 	$each = mysqli_fetch_array($result);
+	$info_array = array("id"=>$each['id'],"name"=>$each['name'],"access"=>$each['access']);
+	$info_string = serialize($info_array);
+	$encrypted_info = openssl_encrypt($info_string,"AES-128-ECB",$key_string);
+	$encrypted_key = openssl_encrypt($key_string,"AES-128-ECB",$encrypted_info);
+	$encrypted_info_2 = openssl_encrypt($encrypted_info,"AES-128-ECB",$encrypted_key);
+	$cookie_value = array($encrypted_key,$encrypted_info_2);
 	session_start();
 	$_SESSION['id'] = $each['id'];
 	$_SESSION['name'] = $each['name'];
 	$_SESSION['access'] = $each['access'];
+	if (!empty($remember)){
+		setcookie('remember', $cookie_value, time() + (86400*30));
+	}
 	header('location:admin_view.php');
 	exit();
 }
